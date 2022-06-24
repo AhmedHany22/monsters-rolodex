58f78f4b3d15dbbe107c56061d92678d32b7b42c
Ahmed Hany <ahmed_hany19@outlook.com>
2022-06-24 14:46:45 +0200

    Search functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ class App extends Component {
     super(props);
     this.state = {
       monsters: [],
+      searchTerm: "",
     };
   }
 
@@ -22,12 +23,25 @@ class App extends Component {
       );
   }
 
+  onSearchChange = (e) => this.setState({ searchTerm: e.target.value });
+
   render() {
+    const { monsters, searchTerm } = this.state;
+    const filteredArray = monsters.filter((user) => {
+      return user.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+
     return (
       <div>
         <h1>All monestrs</h1>
+        <input
+          type="search"
+          className="search-box"
+          placeholder="search monesters"
+          onChange={this.onSearchChange}
+        />
         <ul>
-          {this.state.monsters.map((monster) => (
+          {filteredArray.map((monster) => (
             <li key={monster.id}>{monster.name}</li>
           ))}
         </ul>
